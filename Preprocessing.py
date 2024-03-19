d75647788c41b1b3cbd41feb275f28fb411a65c5
Girish Mahale <girishmahale786@gmail.com>
2024-03-20 00:40:20 +0530

    added requirements

--- Preprocessing.py
+++ Preprocessing.py
@@ -1,20 +1,16 @@
 import pandas as pd
 from sklearn.impute import KNNImputer
 
+
 class Preprocessing:
-    df:pd.DataFrame = None
-    '''
-    Median: Median imputation for missing values in the dataset
-    knn: KNN imputation for missing values in the dataset
-    Mean: Mean imputation for missing values in the dataset
-    '''
-    def remove_nulls(self, option:str) -> None:
-        if option == 'median':
+    df = None
+
+    def remove_nulls(self, option: str) -> None:
+        if option == "median":
             self.df = self.df.fillna(self.df.median())
-        elif option == 'knn':
+        elif option == "knn":
             input_neighbours = int(input("Enter the number of neighbours: "))
             imputer = KNNImputer(n_neighbors=input_neighbours)
             self.df = imputer.fit_transform(self.df)
-        elif option == 'mean':
+        elif option == "mean":
             self.df = self.df.fillna(self.df.mean())
-            
