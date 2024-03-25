451e3c83435dfd3f0c6f51c39ef89e10a1bedd1e
vaibhav7766 <vaibhavsharmav777@gmail.com>
2024-03-26 00:37:24 +0530

    modified:   preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from sklearn.impute import KNNImputer
 
 
 class Preprocessing:
@@ -8,9 +7,28 @@ class Preprocessing:
     def remove_nulls(self, option: str) -> None:
         if option == "median":
             self.df = self.df.fillna(self.df.median())
-        elif option == "knn":
-            input_neighbours = int(input("Enter the number of neighbours: "))
-            imputer = KNNImputer(n_neighbors=input_neighbours)
-            self.df = imputer.fit_transform(self.df)
         elif option == "mean":
             self.df = self.df.fillna(self.df.mean())
+        elif option == "mode":
+            self.df = self.df.fillna(self.df.mode())
+        else:
+            print("Invalid option. Using default: median")
+            self.df = self.df.fillna(self.df.median())
+    
+    def encode(self, option: str) -> None:
+        if option == "onehot":
+            self.df = pd.get_dummies(self.df)
+        elif option == "label":
+            self.df = self.df.apply(lambda x: pd.factorize(x)[0])
+        else:
+            print("Invalid option. Using default: onehot")
+            self.df = pd.get_dummies(self.df)
+    
+    def feature_scaling(self, option: str) -> None:
+        if option == "standard":
+            self.df = (self.df - self.df.mean()) / self.df.std()
+        elif option == "minmax":
+            self.df = (self.df - self.df.min()) / (self.df.max() - self.df.min())
+        else:
+            print("Invalid option. Using default: standard")
+            self.df = (self.df - self.df.mean()) / self.df.std()
